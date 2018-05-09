fb6c32154fe958f08fa60728592054b1188c64fa
tutkuince <tutku.ince@outlook.com>
2018-05-09 19:28:00 +0300

    added commet for description to piece of codes

--- src/io/spring/_01ioc/idealsolution/HelloSpringApp.java
+++ src/io/spring/_01ioc/idealsolution/HelloSpringApp.java
@@ -4,12 +4,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
 	public static void main(String[] args) {
+		
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		// retrieve bean from spring container
 		Coach theCoach = context.getBean("theCoach", Coach.class);
 		
+		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
+		// close the context
 		context.close();
 	}
 }
