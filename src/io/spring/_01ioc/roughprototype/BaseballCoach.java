84ed7ed8addcc3d884c049ea59529c2da900a801
tutkuince <tutku.ince@outlook.com>
2018-05-09 19:15:35 +0300

    Created BaseballCoach class and implements Coach interface

---
+++ src/io/spring/_01ioc/roughprototype/BaseballCoach.java
@@ -0,0 +1,9 @@
+package io.spring._01ioc.roughprototype;
+
+public class BaseballCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 30 minutes on batting practice";
+	}
+}
