8e7c90040b92e54be49f8cfe08f49768f0a77b4c
tutkuince <tutku.ince@outlook.com>
2018-05-11 16:27:11 +0300

    create prototypescope packageand do example about it

--- src/io/spring/_03beanscopes/prototype/PrototypeDemo.java
+++ src/io/spring/_03beanscopes/prototype/PrototypeDemo.java
@@ -1,29 +1,28 @@
-package io.spring._03beanscopes.singleton;
+package io.spring._03beanscopes.prototype;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import io.spring._02di.constructorinjection.Coach;
 import io.spring._02di.setterinjection.CricketCoach;
 
-public class BeanScopeDemo {
+public class PrototypeDemo {
 	public static void main(String[] args) {
-		// Load Spring configuration file 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_03beanScope.singleton.xml");
-		
+		// Load Spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext_03beanScope.prototype.xml");
+
 		// retrieve bean from Spring Container
-		Coach theCoach = context.getBean("theCoach", CricketCoach.class);
-		
 		Coach alphaCoach = context.getBean("theCoach", CricketCoach.class);
-		
+		Coach betaCoach = context.getBean("theCoach", CricketCoach.class);
+
 		// check if they are same
-		boolean result = (theCoach == alphaCoach);
-		
-		// print results
+		boolean result = (alphaCoach == betaCoach);
+
+		// print the result
 		System.out.println("\nPointing the same object: " + result);
-		System.out.println("\nMemory location for theCoach: " + theCoach);
 		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
-		
-		
+		System.out.println("\nMemory location for betaCoach: " + betaCoach);
+
 		// close context
 		context.close();
 	}
